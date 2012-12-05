c146cb2b7494fc55e4e617abd7dc5ab34359380b
Gary Bernstein <garyb@physics.upenn.edu>
2012-12-05 10:39:59 -0500

    Bob's new version of the python script.

--- parse3.py
+++ parse3.py
@@ -1,4 +1,4 @@
-#!/sw/bin/python2.7
+#!/usr/bin/env python
 
 import pyfits
 import glob
@@ -24,11 +24,11 @@ Assigned =['Params','AddFiles','Outfile']
 
 Internal =['FILENAME','EXTENSION','INSTRUMENT','DEVICE',
            'FIELD','EXPOSURE','RA','DEC','WCSFILE','XKEY',
-           'YKEY','IDKEY']
+           'YKEY','IDKEY','EXPID']
 IDefault ={'FILENAME':'','EXTENSION':-1,'INSTRUMENT':'','DEVICE':'',
            'FIELD':'@_NEAREST','EXPOSURE':'','RA':'','DEC':'',
            'WCSFILE':'','XKEY':'',
-           'YKEY':'','IDKEY':'@_ROW'}
+           'YKEY':'','IDKEY':'@_ROW','EXPID':0}
 
 
 def py_to_fits(val):
@@ -78,7 +78,7 @@ class ExposureExt:
     def setVal(self,key,val):
         for item in self.param:
             if key == item.name:
-                if key == 'EXTENSION':
+                if key == 'EXTENSION' or key=='EXPID':
                     item.value=int(val)
                     return
                 item.value=val
@@ -169,23 +169,48 @@ def setInitExp(exps,regex,alist,param):
     """
     # make sure all params are in param list
     #refmatch=re.compile(r'\${(.*)}')
-    refmatch='\${(\w*)}'
+    refmatch='\${(\S*?)}'
+    regexmatch='(\S*):(\S*)'
+    rangematch=r'\((\d+),(\d+)\)'
+
     for key,val in param.items():
+
+        # Look for single references
         match_list=re.findall(refmatch,val)
-        for m in match_list: 
+        for m in match_list:
+            reg_match= re.search(regexmatch,m)
+            if reg_match:
+                m=reg_match.group(1)
+                
             if m not in alist and m not in Internal :
                 raise ValueError('Cannot set '+
                                  m+' not in Params list')
             continue
+
         if key not in alist and key not in Internal :
-            raise ValueError('Cannot set '+key+' not in Params list')
+            raise ValueError('Cannot set '+key+' not in Params list')            
 
     #head=re.compile(r'Header')
     filematch=re.compile(regex)
 
 
+    # build range match
+    idlist=[]
+    rm=re.search(rangematch,regex)
+    if rm:
+        
+        start=int(rm.group(1))
+        end=int(rm.group(2))
+        if end<start:
+            raise ValueError('Cannot set range '+regex+' first > last')            
+        idlist=range(start,end)
+        
+
     for exp in exps:
-        if not filematch.search(exp.getVal('FILENAME')): continue
+        if not idlist:
+            if not filematch.search(exp.getVal('FILENAME')): continue
+        else:
+            if exp.getVal('EXPID') not in idlist:continue
         
         # list for parameters that reference other parameters
         # the first entry in map will be the reference parameter
@@ -193,8 +218,10 @@ def setInitExp(exps,regex,alist,param):
         wait={}#defaultdict(list)
         for key,val in param.items():
             match_list=re.findall(refmatch,val)
+
             if len(match_list)>0:
                 wait[key]=val
+                continue
             #    sub_string=val
             #    for i,m in enumerate(match_list):
             #        sub_string=re.sub(m,'XXXX%d'%i,sub_string)
@@ -206,8 +233,27 @@ def setInitExp(exps,regex,alist,param):
         for key,val in wait.items():
             match_list=re.findall(refmatch,val)
             for m in match_list:
-                par_val=str(exp.getVal(m))
-                val=re.sub('\${%s}'%m,par_val,val)
+                
+                # Look for a regex
+                reg_match= re.search(regexmatch,m)
+                if reg_match:
+                    # assign m to the first entry
+                    localkey=reg_match.group(1)
+                    regex=reg_match.group(2)
+                    
+                    par_val=str(exp.getVal(localkey))
+                    
+                    match=re.search(regex,par_val)
+                    if not match:
+                        raise ValueError('Cannot match regex '+regex+
+                                         'to '+par_val)
+                    par_val=match.group(1)
+
+                else:
+                    par_val=str(exp.getVal(m))
+
+                val=val.replace('${%s}'%m,par_val)
+                
             exp.setVal(key,val)
 
 def buildExpList(flist):
@@ -220,11 +266,14 @@ def buildExpList(flist):
 
     for exp in flist:
         basefile=os.path.basename(exp.getVal('FILENAME'))
+
+        # this removes any extension like .fits
         m=re.search(r'(.*)\.\S*\b',basefile)
 
         if not m:
-            raise ValueError('Cannot match name from file: '+file)
+            raise ValueError('Cannot match file no extension: '+file)
         name=m.group(1)
+        
         # remove _cat if it is there
         name=re.sub('_cat','',name)
 
@@ -245,6 +294,13 @@ map,filelist,global_p,local_p=getConfig(sys.argv[1])
 
 if 'Params' in map:
     eparams=re.split(',',map['Params'])
+
+# Found strange error if params has internal params listed there
+# need to remove them
+for par in eparams:
+    if par in Internal:
+        print par+' already listed as internal parameter.  Removing...'
+        eparams.remove(par)
 
 # get file list that match
 files=[]
@@ -267,14 +323,20 @@ for file in files:
 exps=buildExpList(flist)
 
 setInitExp(flist,'.*',eparams,global_p)
+wait={}
 for regex,p in local_p.items():
+    if regex[0]=='(':
+        wait[regex]=p
+        continue
+    setInitExp(flist,regex,eparams,p)
+
+for regex,p in wait.items():
     setInitExp(flist,regex,eparams,p)
 
 
 
-
-
 vars=deepcopy(Internal)
+
 # in case we want to remove any internal parameters we can do
 # it here
 vars.extend(eparams)
@@ -285,6 +347,8 @@ for exp in exps:
         val=exp.getVal(v)
         vals[v].append(val)
 
+if max(vals['EXPID'])==0:
+    del vals['EXPID']
 
 
 cols=[]
