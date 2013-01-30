b7fba48aa4fe4bc27efde2cc31ff2bf0de025df4
Gary Bernstein <garyb@physics.upenn.edu>
2013-01-29 21:21:26 -0500

    ApplyWCS is rewritten to use serialized files.  Compiles and runs.  Might want to change output precision.

--- ApplyWCS.cpp
+++ ApplyWCS.cpp
@@ -4,21 +4,24 @@
 #include <map>
 
 #include "Std.h"
-#include "Astrometry.h"
-#include "TPVMap.h"
-#include "Header.h"
 #include "StringStuff.h"
-
+#include "Astrometry.h"
+#include "PixelMapCollection.h"
+#include "Header.h"
+#include "TPVMap.h"
 using namespace std;
 using namespace astrometry;
 
 string usage="ApplyWCS: use WCS systems specified by FITS keywords to map from one\n"
              "   pixel system to another, or from pixel system to/from world coords\n"
-             "usage: ApplyWCS <WCS in> <WCS out> [ra dec]\n"
-	     "      <WCS in, out>:  each is either (a) name of file holding FITS-style WCS spec,\n"
-             "             (b) the word \"gnomonic\" followed by RA and Dec (in hours, degrees)\n"
+             "usage: ApplyWCS <WCS in> <WCS out> \n"
+	     "      <WCS in, out>:  each is either:\n"
+             "             (a) name of file holding FITS-style WCS spec,\n"
+             "             (b) <wcsname>@<filename>, reads WCS with given name serialized in the file\n"
+             "             (c) the word \"gnomonic\" followed by RA and Dec (in hours, degrees)\n"
              "                 that will be pole of a projection to/from (xi,eta) in degrees\n"
-             "             (c) a dash - to use RA and Dec directly (in hours, degrees).\n"
+             "             (d) either \"icrs\" or \"ecliptic\" to use true sky coords in degrees\n"
+             "             (e) a dash - to get input/output in the native projection of the Wcs.\n"
              "     stdin: each line is <xin> <yin> which are either input pixel or input WCS coords\n"
              "     stdout: each line is <xout> <yout> for either output pixel system or WCS";
 
@@ -31,141 +34,142 @@ main(int argc, char *argv[])
       cerr << usage << endl;
       exit(1);
     }
-    Wcs* wcsIn = 0;
     int iarg=1;
-    string wcsfilein = argv[iarg];
-    iarg++;
-    SphericalCoords* inProjection = 0;
+    vector<Wcs*> wcs(2, (Wcs*) 0);
+    vector<SphericalCoords*> projection(2, (SphericalCoords*) 0);
+    vector<bool> useNative(2,false);
 
-    // Choose input method:
-    if (stringstuff::nocaseEqual(wcsfilein, "gnomonic")) {
-      if (argc < iarg+3) {
-	cerr << "Not enough command line arguments after gnomonic input" << endl;
+    for (int i=0; i<2; i++) {
+      // Choose input (i=0) / output (i=1) system:
+      if (iarg >= argc) {
+	cerr << "Not enough command line arguments."  << endl;
+	cerr << usage << endl;
 	exit(1);
       }
-      string radecString = argv[iarg];
-      radecString += " ";
-      radecString += argv[iarg+1];
-      iarg += 2;
-      SphericalICRS pole;
-      istringstream iss(radecString);
-      if (!(iss >> pole)) {
-	cerr << "Bad RA/Dec: " << radecString << endl;
-	exit(1);
-      }
-      Orientation orient(pole);
-      inProjection = new Gnomonic(orient);
-    } else if (wcsfilein=="-") {
-      // Nothing to do, will just read RA and Dec
-    } else {
-      // Get WCS from file:
-      ifstream mapfs(wcsfilein.c_str());
-      if (!mapfs) {
-	cerr << "Could not open map spec file " << wcsfilein << endl;
-	exit(1);
-      }
-      img::Header h; 
-      mapfs >> h;
-      wcsIn = readTPV(h);
-      if (!wcsIn) {
-	cerr << "Error reading input WCS map from file " << wcsfilein
-	     << endl;
-	exit(1);
+
+      string wcsfile = argv[iarg++];
+
+      if (stringstuff::nocaseEqual(wcsfile, "gnomonic")) {
+	if (iarg+1 >= argc) {
+	  cerr << "Not enough command line arguments after gnomonic input" << endl;
+	  exit(1);
+	}
+	string radecString = argv[iarg++];
+	radecString += " ";
+	radecString += argv[iarg++];
+	SphericalICRS pole;
+	istringstream iss(radecString);
+	if (!(iss >> pole)) {
+	  cerr << "Bad RA/Dec: " << radecString << endl;
+	  exit(1);
+	}
+	Orientation orient(pole);
+	projection[i] = new Gnomonic(orient);
+      } else if (stringstuff::nocaseEqual(wcsfile, "icrs")) {
+	projection[i] = new SphericalICRS;
+      } else if (stringstuff::nocaseEqual(wcsfile, "ecliptic")) {
+	projection[i] = new SphericalEcliptic;
+      } else if (wcsfile=="-") {
+	useNative[i] = true;
+      } else if (wcsfile.find('@')!=string::npos) {
+	// Deserialize WCS from a file
+	size_t at = wcsfile.find('@');
+	string wcsname = wcsfile.substr(0,at);
+	at++;
+	if (at >= wcsfile.size()) {
+	  cerr << "Missing filename in wcsname@filename argument: " << wcsfile << endl;
+	  exit(1);
+	}
+	string filename = wcsfile.substr(at);
+	ifstream ifs(filename.c_str());
+	if (!ifs) {
+	  cerr << "Could not open serialized WCS file " << filename << endl;
+	  exit(1);
+	}
+	PixelMapCollection pmc;
+	if (!pmc.read(ifs)) {
+	  cerr << "File <" << filename << "> is not serialized WCS file" << endl;
+	  exit(1);
+	}
+	wcs[i] = pmc.cloneWcs(wcsname);
+      } else {
+      // Get WCS as TPV in FITS-header-style file
+	ifstream mapfs(wcsfile.c_str());
+	if (!mapfs) {
+	  cerr << "Could not open TPV spec file " << wcsfile << endl;
+	  exit(1);
+	}
+	img::Header h; 
+	mapfs >> h;
+	wcs[i] = readTPV(h);
+	if (!wcs[i]) {
+	  cerr << "Error reading input TPV map from file " << wcsfile << endl;
+	  exit(1);
+	}
       }
     }
 
-    if (argc < iarg+1) {
-      cerr << "Not enough command line arguments" << endl;
+    if ( useNative[0] && !wcs[1] || useNative[1] && !wcs[0]) {
+      cerr << "If you ask for native projection with \"-\" argument, other arg must be a WCS" << endl;
       exit(1);
-    }
-    Wcs* wcsOut = 0;
-    string wcsfileout = argv[iarg];
-    iarg++;
-    SphericalCoords* outProjection = 0;
-
-    // Choose output method:
-    if (stringstuff::nocaseEqual(wcsfileout, "gnomonic")) {
-      if (argc < iarg+2) {
-	cerr << "Not enough command line arguments after gnomonic input" << endl;
-	exit(1);
-      }
-      string radecString = argv[iarg];
-      radecString += " ";
-      radecString += argv[iarg+1];
-      iarg += 2;
-      SphericalICRS pole;
-      istringstream iss(radecString);
-      if (!(iss >> pole)) {
-	cerr << "Bad RA/Dec: " << radecString << endl;
-	exit(1);
-      }
-      Orientation orient(pole);
-      outProjection = new Gnomonic(orient);
-    } else if (wcsfileout=="-") {
-      // Nothing to do, will just read RA and Dec
-    } else {
-      // Get WCS from file:
-      ifstream mapfs(wcsfileout.c_str());
-      if (!mapfs) {
-	cerr << "Could not open map spec file " << wcsfileout << endl;
-	exit(1);
-      }
-      img::Header h; 
-      mapfs >> h;
-      wcsOut = readTPV(h);
-      if (!wcsOut) {
-	cerr << "Error reading output WCS map from file " << wcsfileout
-	     << endl;
-	exit(1);
-      }
     }
 
     // Begin reading data
     string buffer;
     while (stringstuff::getlineNoComment(cin, buffer)) {
-      double xpix;
-      double ypix;
+      double xin;
+      double yin;
+      double xout = 0.;
+      double yout = 0.;
       istringstream iss(buffer);
-      if (!(iss >> xpix >> ypix)) {
+      if (!(iss >> xin >> yin)) {
 	cerr << "Bad input entry <" << buffer
 	     << endl;
 	exit(1);
       }
       // Input coordinates to world system:
       SphericalICRS icrs;
-      if (wcsIn)
-	icrs.convertFrom(wcsIn->toSky(xpix, ypix));
-      else if (inProjection) {
-	inProjection->setLonLat(xpix*DEGREE, ypix*DEGREE);
-	icrs.convertFrom(*inProjection);
-      } else
-	icrs.setLonLat(xpix*DEGREE/15., ypix*DEGREE);
-
-      // Now choose output quantities
-      if (wcsOut) {
-	xpix = ypix = 0.;
-	wcsOut->fromSky(icrs, xpix, ypix);
-      } else if (outProjection) {
-	outProjection->convertFrom(icrs);
-	outProjection->getLonLat(xpix, ypix);
-	xpix /= DEGREE; ypix /= DEGREE;
+      if (wcs[0]) {
+	if (useNative[1]) {
+	  // Just map input to native system of the input Wcs
+	  wcs[0]->getMap()->toWorld(xin, yin, xout, yout);
+	  cout << xout << " " << yout << endl;
+	  continue;
+	} else {
+	  icrs.convertFrom(wcs[0]->toSky(xin, yin));
+	}
+      } else if (projection[0]) {
+	projection[0]->setLonLat(xin*DEGREE, yin*DEGREE);
+	icrs.convertFrom(*projection[0]);
+      } else if (useNative[0]) {
+	// go straight to output system's pixels
+	wcs[1]->getMap()->toPix(xin, yin, xout, yout);
+	cout << xout << " " << yout << endl;
+	continue;
       } else {
-	icrs.getLonLat(xpix, ypix);
-	xpix /= DEGREE*15.;
-	ypix /= DEGREE;
+	cerr << "Logic error." << endl;
+	exit(1);
       }
-      cout << xpix << " " << ypix << endl;
 
-      string appendix;
-      while(iss >> appendix) {
-          cout << " " << appendix;
+      // Now convert to output
+      if (wcs[1]) {
+	wcs[1]->fromSky(icrs, xout, yout);
+      } else if (projection[1]) {
+	projection[1]->convertFrom(icrs);
+	projection[1]->getLonLat(xout, yout);
+	xout /= DEGREE; yout /= DEGREE;
+      } else {
+	cerr << "Logic error." << endl;
+	exit(1);
       }
-      cout << endl;
+      cout << xout << " " << yout << endl;
     }
-    if (wcsIn) delete wcsIn;
-    if (wcsOut) delete wcsOut;
-    if (inProjection) delete inProjection;
-    if (outProjection) delete outProjection;
+
+    // Clean up
+    for (int i=0; i<2; i++) {
+      if (wcs[i]) delete wcs[i];
+      if (projection[i]) delete projection[i];
+    }
   } catch (std::runtime_error &m) {
     quit(m,1);
   }
