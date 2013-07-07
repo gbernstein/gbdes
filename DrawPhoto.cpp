9bd9b31e0ef5184bb9bbb4f27e88cd84f0d45433
Gary Bernstein <garyb@physics.upenn.edu>
2013-07-07 18:08:48 -0400

    Adjusts array to be centered on image.

--- DrawPhoto.cpp
+++ DrawPhoto.cpp
@@ -1,6 +1,8 @@
 // Make a FITS image of a DECam photometric solution, with flat normalization factors taken out.
 #include <fstream>
 #include <sstream>
+#include <map>
+
 #include "StringStuff.h"
 
 #include "FitsImage.h"
@@ -14,7 +16,6 @@ using photometry::PhotoMap;
 
 class Device {
 public:
-  string name;
   Bounds<double> b;
   double norm;
 };
@@ -30,7 +31,7 @@ main(int argc, char *argv[])
   Bounds<double> bDevice(11., 2038., 1., 4096.);
 
   // Read in the device information
-  list<Device> devices;
+  map<string,Device> devices;
   Bounds<double> bExpo;
   {
     ifstream ifs("ccdInfo_i.txt");
@@ -44,11 +45,10 @@ main(int argc, char *argv[])
 	exit(1);
       }
       Device d;
-      d.name = name;
       d.norm = norm;
       d.b = Bounds<double>(xmin, xmax, ymin, ymax);
       bExpo += d.b;
-      devices.push_back(d);
+      devices[name] = d;
     }
   }
 
@@ -58,10 +58,6 @@ main(int argc, char *argv[])
 		      static_cast<int> (floor(bExpo.getYMin()/pixelScale)),
 		      static_cast<int> (ceil(bExpo.getYMax()/pixelScale)) );
   Image<> starflat(bImage);
-
-  /**/cerr << "degrees: " << bExpo 
-	   << " Image: " << bImage
-	   << endl;
 
   // Read in astrometric and photometric solutions
   PixelMapCollection astromaps;
@@ -79,25 +75,39 @@ main(int argc, char *argv[])
   //  string exposurePrefix = "DECam_00163584/";
   string exposurePrefix = "DECam_00163609/";
 
-  for (list<Device>::iterator i = devices.begin();
+  // Adjust coordinates such that mean of N4 and S4 centers is at origin.
+  Position<double> center;
+  {
+    Bounds<double> b;
+    double x,y;
+    astromaps.issueWcs(exposurePrefix + "N4")->toWorld(1024.,2048.,x,y);
+    b += Position<double>(x,y);
+    astromaps.issueWcs(exposurePrefix + "S4")->toWorld(1024.,2048.,x,y);
+    b += Position<double>(x,y);
+    center = b.center();
+    cout << "Center: " << center*3600. << " arcsec" << endl;
+  }
+
+  for (map<string,Device>::iterator i = devices.begin();
        i != devices.end();
        ++i) {
     // Convert device boundaries into pixel range
-    Bounds<int> bpix( static_cast<int> (floor(i->b.getXMin()/pixelScale)),
-		      static_cast<int> (ceil(i->b.getXMax()/pixelScale)),
-		      static_cast<int> (floor(i->b.getYMin()/pixelScale)),
-		      static_cast<int> (ceil(i->b.getYMax()/pixelScale)) );
+    Bounds<double> b = i->second.b;
+    Bounds<int> bpix( static_cast<int> (floor(b.getXMin()/pixelScale)),
+		      static_cast<int> (ceil(b.getXMax()/pixelScale)),
+		      static_cast<int> (floor(b.getYMin()/pixelScale)),
+		      static_cast<int> (ceil(b.getYMax()/pixelScale)) );
 
     // Get Wcs and photometric solution
-    Wcs* devWcs = astromaps.issueWcs(exposurePrefix + i->name);
-    photometry::SubMap* devPhoto = photomaps.issueMap(exposurePrefix + i->name);
+    Wcs* devWcs = astromaps.issueWcs(exposurePrefix + i->first);
+    photometry::SubMap* devPhoto = photomaps.issueMap(exposurePrefix + i->first);
 
     // Loop over pixels
     for (int iy = bpix.getYMin(); iy <= bpix.getYMax(); iy++)
       for (int ix = bpix.getXMin(); ix <= bpix.getXMax(); ix++) {
 	photometry::PhotoArguments args;
-	args.xExposure = ix * pixelScale;
-	args.yExposure = iy * pixelScale;
+	args.xExposure = ix * pixelScale + center.x;
+	args.yExposure = iy * pixelScale + center.y;
 
 	// Map pixel coordinates to exposure and device coordinates
 	devWcs->toPix(args.xExposure, args.yExposure, args.xDevice, args.yDevice);
@@ -108,11 +118,10 @@ main(int argc, char *argv[])
 	double photo = devPhoto->forward(0., args);
 
 	// Apply normalization correction
-	starflat(ix, iy) = pow(10., -0.4*photo) * i->norm; // ??? divide or multiply?
+	starflat(ix, iy) = pow(10., -0.4*photo) * i->second.norm;
       }
 
   } // end Device loop.
   starflat.shift(1,1);
   FitsImage<>::writeToFITS("test.fits", starflat, 0);
 }
-
