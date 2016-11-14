938dc44472e47c323912aa78e81d6136effb56f9
Gary Bernstein <garyb@physics.upenn.edu>
2016-11-14 09:31:02 -0500

    Fix compresscat.py to work with files that have a CONTINUE line in their headers.

--- compresscat.py
+++ compresscat.py
@@ -15,6 +15,7 @@ import fitsio
 import re
 import argparse
 import numpy as np
+from gmbpy import clippedMean
 
 parser = argparse.ArgumentParser(description='Compress and process SExtractor catalogs for high-quality stars')
 parser.add_argument("incat", help='Filename of input catalog', type=str)
@@ -49,8 +50,12 @@ autocorrs = [] # For MAG_AUTO
 psfcorrs = [] # For MAG_PSF
 fluxrads = [] # Record FLUX_RADIUS values
 
+# And these are chip-by-chip medians
+ccd_ap09 = []
+ccd_fluxrad = []
+
 # These header keywords are not to be propagated from the LDAC_HEADER
-skipLDAC = {'SIMPLE','BITPIX','NAXIS','NAXIS1','NAXIS2','EXTEND'}
+skipLDAC = {'SIMPLE','BITPIX','NAXIS','NAXIS1','NAXIS2','EXTEND','CONTINUE'}
 
 # These header keywords are not to be propagated from the .head file
 skipHead = {'FGROUPNO','ASTINST','FLXSCALE','MAGZEROP',
@@ -93,7 +98,9 @@ for i in range(2,len(fitsin),2):
     use = np.logical_and(use, np.logical_not(data['IMAFLAGS_ISO'] & args.imaflags_mask))
 
     tab = data[use]
-
+    if len(tab)==0:
+        continue   # Move to next header if no data here.
+    
     # Calculate median aperture corrections per CCD
     # First find number of apertures used - must be the same
     # for all CCDs
@@ -123,13 +130,15 @@ for i in range(2,len(fitsin),2):
         ac = np.median(apcorr, axis=0)
         for i in range(naper):
             hdr['APCOR{:02d}'.format(i)] = ac[i]
+        if naper>9:
+            ccd_ap09.append(ac[9])
         hdr['APCORAUT'] = np.median(autocorrs[-1])
         if len(psfcorrs)>0:
             hdr['APCORPSF'] = np.median(psfcorrs[-1])
         # And median FLUX_RADIUS:
         if len(fluxrads) > 0:
             hdr['FLUXRAD'] = np.median(fluxrads[-1])
-            
+            ccd_fluxrad.append(np.median(fluxrads[-1]))
     hdrs.append(hdr)
     tabs.append(tab)
 
@@ -137,11 +146,25 @@ for i in range(2,len(fitsin),2):
 ac = np.median(np.concatenate(apcorrs), axis=0)
 for i in range(naper):
     phdr['APCOR{:02d}'.format(i)] = ac[i]
-    phdr['APCORAUT'] = np.median(np.concatenate(autocorrs))
-    if len(psfcorrs)>0:
-        phdr['APCORPSF'] = np.median(np.concatenate(psfcorrs))
-    if len(fluxrads)>0:
-        phdr['FLUXRAD'] = np.median(np.concatenate(fluxrads))
+phdr['APCORAUT'] = np.median(np.concatenate(autocorrs))
+if len(psfcorrs)>0:
+    phdr['APCORPSF'] = np.median(np.concatenate(psfcorrs))
+if len(fluxrads)>0:
+    phdr['FLUXRAD'] = np.median(np.concatenate(fluxrads))
+if len(ccd_fluxrad) > 20:
+    mean,var,n = clippedMean(np.array(ccd_fluxrad),3.)
+    phdr['FRAD_RMS'] = np.sqrt(var)
+else:
+    phdr['FRAD_RMS'] = 0.
+if len(ccd_ap09) > 20:
+    mean,var,n = clippedMean(np.array(ccd_ap09),3.)
+    phdr['AP09_RMS'] = np.sqrt(var)
+else:
+    phdr['AP09_RMS'] = 0.
+    
+# Dump some basic information
+print "{:6d} {:12.6f} {:5.1f} {:s} {:s} {:1s} {:+6.4f} {:6.4f} {:5.3f} {:5.3f}".format(hdr['EXPNUM'],hdr['MJD-OBS'],hdr['EXPTIME'],hdr['TELDEC'],hdr['HA'],hdr['BAND'].strip(),phdr['APCOR09'],phdr['AP09_RMS'],phdr['FLUXRAD']*2*0.264,phdr['FRAD_RMS']*2.*0.264)
+
 # Make an output FITS and save
 out = fitsio.FITS(args.outcat, 'rw', clobber=True)
 for d,h in zip(tabs,hdrs):
