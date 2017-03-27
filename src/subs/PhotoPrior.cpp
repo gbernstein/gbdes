30ff5ac66873e932bd4262fc84d422641562f4f6
Gary Bernstein <garyb@physics.upenn.edu>
2017-03-26 21:15:56 -0400

    Updated PhotoPrior

--- src/subs/PhotoPrior.cpp
+++ src/subs/PhotoPrior.cpp
@@ -64,10 +64,8 @@ PhotoPrior::setParams(const DVector& p) {
 void
 PhotoPrior::countFit() {
   nFit = 0;
-  for (list<PhotoPriorReferencePoint>::const_iterator i = points.begin();
-       i != points.end();
-       ++i) {
-    if (!(i->isClipped)) nFit++;
+  for (auto& i : points) {
+    if (!(i.isClipped)) nFit++;
   }
 }
 
@@ -77,14 +75,12 @@ PhotoPrior::chisq(int& dof) const {
   if (isDegenerate()) return 0.;
 
   double chi = 0.;
-  for (list<PhotoPriorReferencePoint>::const_iterator i = points.begin();
-       i != points.end();
-       ++i) {
-    if (i->isClipped) continue;
-    double dm = i->magOut - m
-      - a*(i->airmass-1.)
-      - b*i->args.color
-      - c*i->apcorr;
+  for (auto& i : points) {
+    if (i.isClipped) continue;
+    double dm = i.magOut - m
+      - a*(i.airmass-1.)
+      - b*i.args.color
+      - c*i.apcorr;
     chi += dm * dm;
   }
   dof += nFit - nFree;
@@ -93,99 +89,92 @@ PhotoPrior::chisq(int& dof) const {
 
 void
 PhotoPrior::remap() {
-  for (list<PhotoPriorReferencePoint>::iterator i = points.begin();
-       i != points.end();
-       ++i) 
-    i->magOut = i->map->forward(i->magIn, i->args);
+  for (auto& i : points) 
+    i.magOut = i.map->forward(i.magIn, i.args);
 }
   
 int
 PhotoPrior::accumulateChisq(double& chisq,
 			    DVector& beta,
-			    astrometry::AlphaUpdater& updater) {
+			    SymmetricUpdater& updater,
+			    bool reuseAlpha) {
   if (isDegenerate()) return 0; // Can't use this Prior if it's degenerate.
 
   int nP = beta.size();
   double wt = 1./(sigma*sigma);
 
   // Update mapping and save derivatives for each detection:
-  for (list<PhotoPriorReferencePoint>::iterator i=points.begin();
-       i!=points.end(); 
-       ++i) {
-    if (i->isClipped) continue;
-    int npi = i->map->nParams();
+  for (auto& i : points) {
+    if (i.isClipped) continue;
+    int npi = i.map->nParams();
     DVector derivs1(npi);
     if (npi>0) {
-      i->magOut = i->map->forwardDerivs(i->magIn, i->args, derivs1);
+      i.magOut = i.map->forwardDerivs(i.magIn, i.args, derivs1);
     } else {
-      i->magOut = i->map->forward(i->magIn, i->args);
+      i.magOut = i.map->forward(i.magIn, i.args);
     }
-    double dm = i->magOut - m
-      - a*(i->airmass-1.)
-      - b*i->args.color
-      - c*i->apcorr;
+    double dm = i.magOut - m
+      - a*(i.airmass-1.)
+      - b*i.args.color
+      - c*i.apcorr;
 
     // Here are the derivs with respect to the prior's parameters:
     DVector derivs2(nParams());
     int index = 0;
     if (mIsFree) derivs2[index++] = -1.;
-    if (aIsFree) derivs2[index++] = -(i->airmass-1.);
-    if (bIsFree) derivs2[index++] = -i->args.color;
-    if (cIsFree) derivs2[index++] = -i->apcorr;
+    if (aIsFree) derivs2[index++] = -(i.airmass-1.);
+    if (bIsFree) derivs2[index++] = -i.args.color;
+    if (cIsFree) derivs2[index++] = -i.apcorr;
       
     chisq += dm * dm * wt;
 
     // Accumulate derivatives:
     int istart=0;  // Index into this point's derivative vector
-    for (int iMap=0; iMap<i->map->nMaps(); iMap++) {
-      int ip=i->map->startIndex(iMap);
-      int np=i->map->nSubParams(iMap);
+    for (int iMap=0; iMap<i.map->nMaps(); iMap++) {
+      int ip=i.map->startIndex(iMap);
+      int np=i.map->nSubParams(iMap);
       if (np==0) continue;
-      int mapNumber1 = i->map->mapNumber(iMap);
+      int mapNumber1 = i.map->mapNumber(iMap);
 
       // Derivs for just this part of the submap:
-      tmv::VectorView<double> sub1=derivs1.subVector(istart,istart+np);
-      beta.subVector(ip, ip+np) -= wt*dm*sub1;
+      DVector sub1=derivs1.subVector(istart,istart+np);
+      beta.subVector(ip, ip+np) -= (wt*dm)*sub1;
 
-      // Augment the portion of alpha straddling diagnonal:
-      //**      alpha.subSymMatrix(ip, ip+np) += wt*(sub1 ^ sub1));
-      updater.rankOneUpdate(mapNumber1, ip, sub1, 
-			    mapNumber1, ip, sub1, wt);
+      if (!reuseAlpha) {
+	// Augment the portion of alpha straddling diagnonal:
+	updater.rankOneUpdate(mapNumber1, ip, sub1, wt);
 
-      // Augment the alpha from cross-derivatives with the prior parameters 
-      if (nParams() > 0) {
-	int mapNumber2 = mapNumber();
-	updater.rankOneUpdate(mapNumber2, startIndex(), derivs2.view(),
-			      mapNumber1, ip, sub1, wt);
+	// Augment the alpha from cross-derivatives with the prior parameters 
+	if (nParams() > 0) {
+	  int mapNumber2 = mapNumber();
+	  updater.rankOneUpdate(mapNumber2, startIndex(), derivs2,
+				mapNumber1, ip, sub1, wt);
+	}
+
+	// Augment alpha with cross-derivatives from other maps' params:
+	int istart2 = istart+np;
+	for (int iMap2=iMap+1; iMap2<i.map->nMaps(); iMap2++) {
+	  int ip2=i.map->startIndex(iMap2);
+	  int np2=i.map->nSubParams(iMap2);
+	  int mapNumber2 = i.map->mapNumber(iMap2);
+	  if (np2==0) continue;
+	  DVector sub2=derivs1.subVector(istart2,istart2+np2);
+	  updater.rankOneUpdate(mapNumber2, ip2, sub2,
+				mapNumber1, ip,  sub1, wt);
+	  istart2+=np2;
+	}
+	istart+=np;
       }
-
-      // Augment alpha with cross-derivatives from other maps' params:
-      int istart2 = istart+np;
-      for (int iMap2=iMap+1; iMap2<i->map->nMaps(); iMap2++) {
-	int ip2=i->map->startIndex(iMap2);
-	int np2=i->map->nSubParams(iMap2);
-	int mapNumber2 = i->map->mapNumber(iMap2);
-	if (np2==0) continue;
-	tmv::VectorView<double> sub2=derivs1.subVector(istart2,istart2+np2);
-	// Note that subMatrix here will not cross diagonal.
-	//**	  alpha.subMatrix(ip, ip+np, ip2, ip2+np2) += wt*(sub1 ^ sub2);
-	updater.rankOneUpdate(mapNumber2, ip2, sub2,
-			      mapNumber1, ip,  sub1, wt);
-	istart2+=np2;
-      }
-      istart+=np;
     } // outer parameter segment loop
-
+    
     // Now add derivs wrt prior's parameters to beta and alpha:
     if (nParams() > 0) {
       int ip = startIndex();
       int np = nParams();
       beta.subVector(ip, ip+np) -= wt*dm*derivs2;
 
-      // Augment the portion of alpha straddling diagnonal:
-      //**      alpha.subSymMatrix(ip, ip+np) += wt*(derivs2 ^ derivs2));
-      updater.rankOneUpdate(mapNumber(), ip, derivs2.view(), 
-			    mapNumber(), ip, derivs2.view(), wt);
+      if (!reuseAlpha)
+	updater.rankOneUpdate(mapNumber(), ip, derivs2, wt);
     }
   } // reference point loop
 
@@ -194,16 +183,14 @@ PhotoPrior::accumulateChisq(double& chisq,
 
 bool
 PhotoPrior::sigmaClip(double sigThresh) {
-  list<PhotoPriorReferencePoint>::iterator worst=points.end();
+  auto worst=points.end();
   double maxDev = sigThresh*sigma;
-  for (list<PhotoPriorReferencePoint>::iterator i=points.begin();
-       i!=points.end(); 
-       ++i) {
+  for (auto i = points.begin(); i!=points.end(); ++i) {
     if (i->isClipped) continue;
     double dm = i->magOut - m
-      - a*(i->airmass-1.)
-      - b*i->args.color
-      - c*i->apcorr;
+      - a *(i->airmass-1.)
+      - b * i->args.color
+      - c * i->apcorr;
     if (abs(dm) > maxDev) {
       worst = i;
       maxDev = abs(dm);
@@ -216,25 +203,20 @@ PhotoPrior::sigmaClip(double sigThresh) {
   worst->isClipped = true;
   nFit--;
   // Will also clip other Points from the same exposure (different color)
-  for (list<PhotoPriorReferencePoint>::iterator i=points.begin();
-       i!=points.end(); 
-       ++i) {
+  for (auto i=points.begin(); i!=points.end(); ++i) {
     if (i==worst || i->isClipped) continue;
     if (i->exposureName == worst->exposureName) {
       i->isClipped = true;
       nFit--;
-  }  
+    }  
   }
   return true;
 }
 
 void
 PhotoPrior::clipAll() {
-  for (list<PhotoPriorReferencePoint>::iterator i=points.begin();
-       i!=points.end(); 
-       ++i)
-    i->isClipped = true;
-
+  for (auto& i : points) 
+    i.isClipped = true;
   nFit = 0;
 }
 
@@ -273,19 +255,17 @@ PhotoPrior::report(ostream& os) const {
      << " " << (apcorrIsFree() ? " " : "*" )
      << endl;
 
-  for (list<PhotoPriorReferencePoint>::const_iterator i = points.begin();
-       i != points.end();
-       ++i) {
-    double resid = i->magOut -
-      (m + a*(i->airmass-1.) + b*i->args.color + c*i->apcorr);
-    os << (i->isClipped ? "- " : "+ ")
-       << setw(12) << left << i->exposureName
-       << " " << setw(8) << i->deviceName
+  for (auto& i : points) {
+    double resid = i.magOut -
+      (m + a*(i.airmass-1.) + b*i.args.color + c*i.apcorr);
+    os << (i.isClipped ? "- " : "+ ")
+       << setw(12) << left << i.exposureName
+       << " " << setw(8) << i.deviceName
        << " " << right << showpos << setprecision(3) << setw(6) << resid
-       << " " << noshowpos << setprecision(3) << setw(7) << i->magIn
-       << " " << setprecision(3) << setw(5) << i->airmass
-       << " " << showpos << setprecision(3) << setw(6) << i->args.color
-       << " " << showpos << setprecision(3) << setw(6) << i->apcorr
+       << " " << noshowpos << setprecision(3) << setw(7) << i.magIn
+       << " " << setprecision(3) << setw(5) << i.airmass
+       << " " << showpos << setprecision(3) << setw(6) << i.args.color
+       << " " << showpos << setprecision(3) << setw(6) << i.apcorr
        << endl;
   }
 }
