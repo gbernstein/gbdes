de7b80fc15ccce30de7e7f7fa43b2d49dd4ed5f1
Gary Bernstein <garyb@physics.upenn.edu>
2017-04-06 21:51:34 -0400

    writing allfit.py

--- src/allfit.py
+++ src/allfit.py
@@ -2,130 +2,135 @@
 """
 Python script for doing photometric fits given a set of catalogs
 from starflat sequences.
-This version uses existing color catalogs and does only requested bands
 """
-
-steps = ("begin", "config", "fof", "final", "draw","end")
-pieces = False # True to fit piecewise edge functions, False for known templates
-fixEdges = True # Set True to hold coefficients of edge templates to 1.
-usage = "Script to run photometric fitting process\n"\
-        "usage: bandPhoto.py <run name> <bands> [beginStep] [endStep]\n"\
-        "   <run name> is base name for files in this run. <run name>.yaml\n"\
-        "      file should give configuration.\n"\
-        "   <bands> is comma-separated list of bands to process.\n"\
-        "   [beginStep]  is name of first step in process to execute.Defaults to 'begin'\n"\
-        "   [endStep]    is name of last step in process to execute. Defaults to 'end'\n"\
-        "Here are the steps: " + str(steps)
 import subprocess
 import os
+import os.path
 import sys
 
-configDir= "/data3/DECAM/PHOTCAL/STARFLATS"
-defaultIn = os.path.join(configDir,'config.yaml')
-astref = "/data3/DECAM/PHOTCAL/STARFLATS/astref.yaml"
-fields = "/data3/DECAM/PHOTCAL/STARFLATS/fields.yaml"
-colors = "/data3/DECAM/PHOTCAL/STARFLATS/colors.yaml"
-atts = "/data3/DECAM/PHOTCAL/STARFLATS/attributes.yaml"
+steps = ("configure", "dcr", "recenter", "match", "photo", "color","astro")
+
+parser = argparse.ArgumentParser(description='Derive photometric and astrometric solutions from a set of detection catalogs')
+parser.add_argument("basename", help='Root name for solution-specific files', type=str)
+parser.add_argument('--setup_dir', help='Directory holding generic setup files', type=str)
+parser.add_argument('--first',help='First step of process to execute',type=str,choices=steps,
+                        default=steps[0])
+parser.add_argument('--last',help='Last step of process to execute',type=str, choices=steps,
+                        default=step[-1])
+parser.add_argument('--bands',help='Comma-separated list of bands for photometry', type=str,
+                        default='g,r,i,z,Y')
+
+args = parser.parse_args()
+
+# First a little routine that keeps track of whether we are in the range of first--last steps
+stepsActive = False   # True if we are to currently executing steps
+stepsDone = False   # True if we are done with all requested steps
+def doStep(stepname):
+    if stepsDone:
+        sys.exit(0)  # past last step
+    if stepname==args.first:
+        stepsActive = True
+    if stepname==args.last:
+        stepsDone = False
+    return stepsActive
+
+# Types of files that will be expected to exist locally:
+specFile = "{:s}.setup".format(parser.basename)
+priorFile = "{:s}.photo.prior".format(parser.basename)
+
+# Parameter and configuration files
+stdSpecs = os.path.join(args.setup_dir,'std.specs')
+fofParams = os.path.join(args.setup_dir,'wcsfof.params')
+photoParams = os.path.join(args.setup_dir,'photofit.params')
+magcolorParams = os.path.join(args.setup_dir,'magcolor.params')
+astroParams = os.path.join(args.setup_dir,'wcsfit.params')
+# Model specifications
+photoInput = os.path.join(args.setup_dir,'nocolor.input.photo')
+colorInput = os.path.join(args.setup_dir,'color.input.photo')
+astroInput = os.path.join(args.setup_dir,'input.astro')
+# Pre-solved model components
+oldPhoto = os.path.join(args.setup_dir,'allsf.guts.photo')
+oldAstro = os.path.join(args.setup_dir,'allsf.guts.astro')
+
+# Files we will create
+configFile = args.basename + '.config'
+configLog = args.basename + '.config.log'
+dcrFile = args.basename + '.dcr.astro'
+fofFile = args.basename + '.fof'
+fofLog = args.basename + '.fof.log'
+photoFile = args.basename + '.{:s}.photo'
+photoCat = args.basename + '.{:s}.photo.fits'
+photoLog = args.basename + '.{:s}.photofit.log'
+priorLog = args.basename + '.{:s}.prior.out'
+colorFile = args.basename + '.gi.cat'
+magFile = args.basename + '.mags'
+magLog = args.basename + '.magcolor.log'
+colorFofFile = args.basename + '.color.fof'
+astroFile = args.basename + '.astro'
+astroLog = args.basename + '.wcsfit.log'
+astroCat = args.basename + '.astro.fits'
 
 
-if len(sys.argv) <3:
-    print usage
-    sys.exit(1)
+bands = set(args.bands.split(','))
 
-run = sys.argv[1]
-configIn = run + '.yaml'
-bands = sys.argv[2].split(',')
-
-configFile = run + ".config"
-fofFile    = run + ".fof"
-
-# Can optionally control which steps of the process are done
-def stepNumber(s):
-    for i,v in enumerate(steps):
-        if s.lower()==v.lower():
-            return i
-    print "Unknown configuration step:",s
-    sys.exit(1)
-
-    
-if len(sys.argv)>3:
-    startStep = stepNumber(sys.argv[3])
-else:
-    startStep = stepNumber("begin")
-if len(sys.argv)>4:
-    endStep = stepNumber(sys.argv[4])
-else:
-    endStep = stepNumber("end")
-    
-    
-if startStep <= stepNumber("config") <= endStep:
+if doStep('config'):    
     # Collect all configuration information
     cmd = ['configure.py']
-    cmd.append(fields)
-    cmd.append(configIn)
-    cmd.append(atts)
-    cmd.append(astref)
-    cmd.append(colors)
+    cmd.append(specfile)
+    if os.path.isfile(stdSpecs):
+        # More attributes, etc., if given:
+        cmd.append(stdSpecs)
     cmd.append(configFile)
-    subprocess.call(cmd)
+    with open(configLog) as log:
+        subprocess.check_call(cmd, stdout=log, stderr=subprocess.STDOUT)
 
-if startStep <= stepNumber("fof") <= endStep:
+if doStep('dcr'):    
+    # Make DCR file
+    cmd = ['decamDCR.py']
+    cmd.append(configFile)
+    cmd.append(dcrFile)
+    subprocess.check_call(cmd, stdout=sys.stdout, stderr=sys.stderr)
+        
+if doStep('recenter'):    
+    # Recenter exposures' RA/Dec
+    cmd = ['recenter.py']
+    cmd.append(configFile)
+    subprocess.check_call(cmd, stdout=sys.stdout, stderr=sys.stderr)
+        
+if doStep("match"):
     # Match objects
     cmd = ["WCSFoF"]
     cmd.append(configFile)
-    cmd.append('-matchradius=2')
-    cmd.append('-minmatch=3')
+    cmd.append(fofParams)
     cmd.append('-outName='+fofFile)
-    subprocess.call(cmd)
+    with open(fofLog) as log:
+        subprocess.check_call(cmd, stdout=log, stderr=subprocess.STDOUT)
 
-
-if startStep <= stepNumber("final") <= endStep:
-    # Photo fitting
-    for b in bands:
+if doStep("photo"):
+    # Photo fitting, without color - do g and i
+    # ???
+    for b in bands & set(['g','i']):
         cmd = ['PhotoFit']
-        cmd.append(fofFile)
-        cmd.append(os.path.join(configDir,'photofit.params'))
-        cmd.append('-useInstruments='+b)
-        cmd.append('-outCatalog={:s}.{:s}.photo.fits'.format(run,b))
-        cmd.append('-outPhotFile={:s}.{:s}.photo'.format(run,b))
-        cmd.append('-outPriorFile={:s}.{:s}.prior.out'.format(run,b))
-        cmd.append('-deviceModel')
-        if pieces:
-            cmd.append('Poly 4 + Color Poly 1 + ' \
-                       'Template photorings3.yaml DEVICE + ' \
-                       'Piecewise X 8 153 8 + ' \
-                       'Piecewise X 1897 2048 8')
+        cmd.append(photoParams)
+        cmd.append('-useInstruments='+b+'.*')  # ?? more complex instruments ??
+        cmd.append('-outCatalog='+photoCat.format(b))
+        cmd.append('-outPriorFile='+priorLog.format(b))
+        cmd.append('-outPhotFile='+photoCat.format(b))
+        cmd.append('-colorExposures=""')
+        if os.path.isfile(oldPhoto):
+            cmd.append('-inputMaps='+photoInput+','+oldPhoto)
         else:
-            cmd.append('Poly 4 + Color Poly 1 + ' \
-                       'Template photorings3.yaml DEVICE + ' \
-                       'Template edges.yaml INST/DEVICE_left + ' \
-                       'Template edges.yaml INST/DEVICE_right')
-            if fixEdges:
-                cmd.append('-fixMaps='+b+'/[NS].*_[34]')
-        cmd.append('-exposureModel')
-        cmd.append('Constant + Color Constant')
-        ###cmd.append('Poly 1 + Color Constant')
-        ###cmd.append('Poly 2 + Color Constant')
-        cmd.append('-colorExposures=gi0640,gi0730,gi1327,gi1900,gi2040')
-               
-        outfile = open('{:s}.{:s}.photofit.out'.format(run,b), 'w')
+            cmd.append('-inputMaps='+photoInput)
+        with open(photoLog.format(b)) as log:
+            subprocess.check_call(cmd, stdout=log, stderr=subprocess.STDOUT)
 
-        subprocess.call(cmd,stdout=outfile)
-        outfile.close()
+if doStep("color"):
+    # MagColor, refit, MagColor again...
+    pass
 
-if startStep <= stepNumber("draw") <= endStep:
-    for b in bands:
-        # Draw the flat with rings and edges stripped out
-        cmd = ('python ' + os.path.join(configDir,'stripRings.py')).split()
-        fin = open('{:s}.{:s}.photo'.format(run,b))
-        stripped = 'tmp.photo'
-        fout = open(stripped,'w')
-        subprocess.call(cmd, stdin=fin, stdout=fout)
-        fin.close()
-        fout.close()
-    
-        cmd = 'DrawPhoto 32'.split()
-        cmd.append(stripped)
-        cmd.append(b)
-        cmd.append('{:s}.starflat_{:s}.fits'.format(run,b))
-        subprocess.call(cmd)
+# ?? Phot2DESDM
+
+if doStep('astro')
+    pass
+
+sys.exit(0)
