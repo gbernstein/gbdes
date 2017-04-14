e93c9041ecbe69c6e3b9150688d8ae2ed265d4e3
Gary Bernstein <garyb@physics.upenn.edu>
2017-04-14 18:16:17 -0400

    ReadPhotoPriors now in YAML and compiles.

--- src/subs/ReadPhotoPriors.cpp
+++ src/subs/ReadPhotoPriors.cpp
@@ -1,23 +1,35 @@
 // Code to read PhotoPriors from files.
 // Used in PhotoFit.cpp.
+// Now reading the priors in YAML format.
 
-// The prior files will be expected to have the following format.
-// * All blank lines or those starting with '#' or ';' are skipped
-// * First line of a prior has 
-//       <name> <sigma>
-// * Then there are lines specifying the initial zeropoint, color coefficent, and airmass coeff
-//   and whether these are to be held fixed or free.  These are given on optional lines saying:
-//   <zeropoint | airmass | color> <value> [free | fixed]
-//   where the exact words are given in 1st & 3rd args.  If 3rd is omitted, quantity is fixed.
-//   Any of these three not specified defaults to a fixed value of zero.
-// * Additional lines describe an exposure & device to be used as the reference point
-//   <Exposure name> <Device name> [xpix ypix] [instrumental mag]
-//   the optional last argument defaults to 0 and is the input mag that has to result in a match
-//   to the others under the prior.  Set it to input value that 1 ADU/s will have, typically.
-//   Also optional is a pixel coordinate pair to determine the reference point on the device.
-//   Defaults to the center of the selected device.
-//   Exposure name can be a regular expression.
-// * A prior ends when the file does, or at a line saying END
+/***
+Recall that the prior constraining residuals to the model
+(m_out - m_in) = zpt + a*(airmass-1) + b*color + c*apcorr
+where the LHS is the shift being applied to cataloged mags
+for some location on some device in some exposure.
+
+In the prior file, YAML root node will be a dict with the
+keys being names of prior constraint on a group of exposures, and the values
+being dictionaries with these keys:
+
+exposures:  one or a list of regexes specifying which exposures to constrain
+sigma:      the uncertainty assigned to (mout-min) in forming chisq (default: 0.001)
+device:     which device on the exposure to use (default: "S2")
+xpixel:     x pixel location to use (default or negative: use ctr of device)
+ypixel:     y pixel location to use (default or negative: use ctr of device)
+
+zeropoint:  initial value for zpt coefficient  
+airmass:    initial value for airmass coefficient
+color:      initial value for color coefficient
+apcorr:     initial value for apcorr coefficient
+ (all default to zero)
+
+free:       list of which of (zeropoint,airmass,color,apcorr)
+coefficients should be allowed to vary during fit.  (default: none)
+
+So the default will be an "absolute" constraint forcing the mag shift to
+zero for the specified exposures.
+***/
 #include <fstream>
 #include <sstream>
 
@@ -27,6 +39,8 @@
 #include "PhotoMatch.h"
 #include "PhotoMapCollection.h"
 #include "FitSubroutines.h"
+#include "yaml-cpp/yaml.h"
+
 using namespace std;
 using namespace stringstuff;
 using namespace photometry;
@@ -35,27 +49,6 @@ void formatError(string filename, string line) {
   cerr << "Format error reading prior file " << filename << " on input line:" << endl;
   cerr << line << endl;
   exit(1);
-}
-
-// Read double value and then possible "fixed" or "free" from stream.
-// Missing fixed/free defaults to fixed.
-// Return true on success.
-bool valueAndFree(istream& is, double& value, bool& free) {
-  if (!(is >> value)) return false;
-  string freeString;
-  if (!(is >> freeString)) {
-    free = false;
-    return true;
-  } else if (nocaseEqual(freeString, "fixed")) {
-    free = false;
-    return true;
-  } else if (nocaseEqual(freeString, "free")) {
-    free = true;
-    return true;
-  } else {
-    // Unknown word:
-    return false;
-  }
 }
 
 list<PhotoPrior*>
@@ -72,188 +65,192 @@ readPriors(string filename,
 
   list<PhotoPrior*> out;
 
-  string buffer;
-  while (getlineNoComment(ifs, buffer)) {
-    string name;
-    double sigma;
-    {
-      istringstream iss(buffer);
-      if (!(iss >> name >> sigma) || sigma<=0.) {
-	cerr << "Error reading prior name & sigma at line: " << buffer << endl;
-	exit(1);
-      }
+  try {
+    YAML::Node root = YAML::Load(ifs);
+    if (!root.IsMap()) {
+      // A valid YAML document that does not have the structure 
+      // that our files should have.
+      throw PhotometryError("Invalid structure for photo prior file " + filename);
     }
+    
+    // Every entry in the root node map is a prior specification
+    for (YAML::const_iterator i = root.begin();
+	 i != root.end();
+	 ++i) {
+      string priorName = i->first.as<string>();
+      const YAML::Node& node=i->second;
+      if (!node.IsMap() || !node["Exposures"]) 
+	throw PhotometryError("Non-map or missing exposures key in prior YAML entry " +
+			      priorName);
 
-    list<PhotoPriorReferencePoint> refs;
-    double zeropoint=0.;
-    double airmassCoeff=0.;
-    double colorCoeff=0.;
-    double apcorrCoeff=0.;
-    bool zpIsFree = false;
-    bool airmassIsFree = false;
-    bool colorIsFree = false;
-    bool apcorrIsFree = false;
-
-    // Keep track of exposures already included in this prior.
-    set<int> includedExposures;
-
-    while (getlineNoComment(ifs,buffer)) {
-      string keyword;
-      double value;
-      string fixedString;
-      istringstream iss(buffer);
-      if (!(iss >> keyword)) 
-	formatError(filename, buffer);
-
-      // "END" marks end of a prior
-      if (nocaseEqual(keyword, "END")) 
-	break;
-      
-      if (nocaseEqual(keyword, "zeropoint")) {
-	if (!valueAndFree(iss, zeropoint, zpIsFree))
-	  formatError(filename, buffer);
-      } else if (nocaseEqual(keyword, "airmass")) {
-	if (!valueAndFree(iss, airmassCoeff, airmassIsFree))
-	  formatError(filename, buffer);
-      } else if (nocaseEqual(keyword, "color")) {
-	if (!valueAndFree(iss, colorCoeff, colorIsFree))
-	  formatError(filename, buffer);
-      } else if (nocaseEqual(keyword, "apcorr")) {
-	if (!valueAndFree(iss, apcorrCoeff, apcorrIsFree))
-	  formatError(filename, buffer);
+      // Read the exposures
+      list<string> exposureRegexes;
+      if (node["Exposures"].IsSequence()) {
+	exposureRegexes = node["Exposures"].as<list<string>>();
       } else {
-	// The keyword should be the name of an exposure.
-	string exposureRegex = keyword;
-	// We will also get name of reference device,
-	string deviceName;
-	// and pixel coords of reference point - device center is default
-	bool useCenterOfDevice = true;
-	double xpix, ypix;
-	// and reference instrumental magnitude
-	double instrumentalMag = 0.;
+	exposureRegexes.push_back(node["Exposures"].as<string>());
+      }
 
-	if (!(iss >> deviceName))
-	  formatError(filename, buffer);
-	if (iss >> xpix >> ypix) {
-	  useCenterOfDevice = false;
-	  iss >> instrumentalMag;
+      // Width of magnitude prior:
+      double sigma = 0.001;
+      if (node["Sigma"])
+	sigma = node["Sigma"].as<double>();
+
+      // Location of reference point:
+      string deviceName = "S2";
+      double xpixel = -1.;
+      double ypixel = -1.;
+      if (node["Device"])
+	deviceName = node["Device"].as<string>();
+      if (node["Xpixel"])
+	xpixel = node["Xpixel"].as<double>();
+      if (node["Ypixel"])
+	ypixel = node["Ypixel"].as<double>();
+
+      // Coefficients
+      double zeropoint=0.;
+      double airmassCoeff=0.;
+      double colorCoeff=0.;
+      double apcorrCoeff=0.;
+      if (node["Zeropoint"]) 
+	sigma = node["Zeropoint"].as<double>();
+      if (node["Airmass"])
+	airmassCoeff = node["Airmass"].as<double>();
+      if (node["Color"])
+	colorCoeff = node["Color"].as<double>();
+      if (node["Apcorr"])
+	apcorrCoeff = node["Apcorr"].as<double>();
+
+
+      // Fixed or free parameters?
+      bool zpIsFree = false;
+      bool airmassIsFree = false;
+      bool colorIsFree = false;
+      bool apcorrIsFree = false;
+      list<string> freeList;
+      if (node["Free"]) {
+	if (node["Free"].IsSequence()) {
+	  freeList = node["Free"].as<list<string>>();
+	} else {
+	  freeList.push_back(node["Free"].as<string>());
 	}
-
-	// Now look for all exposures that match the request here.
-	vector<int> matchingExposures;
-	for (int iExpo = 0; iExpo < exposures.size(); iExpo++) {
-	  if (!exposures[iExpo]) continue;
-	  if (includedExposures.count(iExpo) > 0) continue; // Already have this exposure
-	  //???	  if (detectionsPerExposure[iExpo] <=0) continue; // Exclude no-data exposures
-	  if (regexMatch(exposureRegex, exposures[iExpo]->name))
-	    matchingExposures.push_back(iExpo);
+	for (auto s : freeList) {
+	  if (nocaseEqual(s, "zeropoint"))
+	    zpIsFree = true;
+	  else if (nocaseEqual(s, "airmass"))
+	    airmassIsFree = true;
+	  else if (nocaseEqual(s, "color"))
+	    colorIsFree = true;
+	  else if (nocaseEqual(s, "apcorr"))
+	    apcorrIsFree = true;
+	  else 
+	    throw PhotometryError("Unknown PhotoPrior coefficient <" + s + ">");
 	}
+      }
 
-	// For each matching exposure: look for instrument, device name, 
-	// and get center coords if needed
-	vector<int> matchingDevices;
-	vector<double> matchingXPix;
-	vector<double> matchingYPix;
-	for (auto i = matchingExposures.begin();
-	     i != matchingExposures.end(); ) {
-	  int iExpo = *i;
-	  Assert(exposures[iExpo]);
+      // The input magnitude for each reference point:
+      double instrumentalMag = 0.;
+
+      // Now construct reference points from matching exposures
+      list<PhotoPriorReferencePoint> refs;
+
+      // Now look for all exposures that match the request here.
+      vector<int> matchingExposures;
+      vector<int> matchingDevices;
+      vector<double> matchingXPix;
+      vector<double> matchingYPix;
+      for (int iExpo = 0; iExpo < exposures.size(); iExpo++) {
+	if (!exposures[iExpo]) continue;
+	if (regexMatchAny(exposureRegexes, exposures[iExpo]->name)) {
+	  // For each matching exposure: look for instrument, device name, 
+	  // and get center coords if needed
 	  int iInst = exposures[iExpo]->instrument;
-	  if (iInst<0 || !instruments[iInst]) {
-	    // We will ignore failed exposure specifications and only warn when
-	    // nothing turns up valid at the end.
-	    i = matchingExposures.erase(i);
+	  if (iInst<0 || !instruments[iInst])
 	    continue;
-	  }
 
 	  Instrument& inst = *instruments[iInst];
 	  int iDevice = inst.deviceNames.indexOf(deviceName);
 	  if (iDevice < 0) {
 	    // This instrument has no such device.
-	    i = matchingExposures.erase(i);
 	    continue;
 	  }
 	  // Save away the device's number and the pixel coordinates we will use.
+	  matchingExposures.push_back(iExpo);
 	  matchingDevices.push_back(iDevice);
 	  Position<double> center = inst.domains[iDevice].center();
-	  matchingXPix.push_back( useCenterOfDevice ? center.x : xpix);
-	  matchingYPix.push_back( useCenterOfDevice ? center.y : ypix);
-
-	  ++i;
-	} // end of loop finding matching exposures
-	    
-	// Troll extensions looking for exposure + device match
-	bool foundOne = false;
-	for (int iExtn = 0; iExtn < extensions.size(); iExtn++) {
-	  if (!extensions[iExtn]) continue;
-	  Photo::Extension& extn = *extensions[iExtn];
-	  bool extnMatch = false;
-	  for (int iMatch = 0; !extnMatch && iMatch < matchingExposures.size(); iMatch++) {
-	    if (extn.exposure==matchingExposures[iMatch]
-		&& extn.device==matchingDevices[iMatch]) {
-	      extnMatch = true; // Stop looping - a given extension will match only once
-	      // Note that we've included this exposure in this prior.
-	      includedExposures.insert(matchingExposures[iMatch]);
-	      PhotoPriorReferencePoint point;
-	      point.exposureName = exposures[matchingExposures[iMatch]]->name;
-	      point.deviceName = deviceName;
-	      point.magIn = instrumentalMag;
-	      // Check for airmass if needed
-	      point.airmass = extn.airmass;
-	      if (airmassIsFree && point.airmass < 1.) {
-		cerr << "Prior " << name 
-		     << " uses airmass but airmass<1 at exposure " 
-		     << point.exposureName
-		     << " device " << point.deviceName
-		     << endl;
-		exit(1);
-	      }
-	      point.apcorr = extn.apcorr;
-
-	      // Map the coordinates to get [xy]Exposure
-	      Assert(extn.startWcs);
-	      point.args.xDevice = matchingXPix[iMatch];
-	      point.args.yDevice = matchingYPix[iMatch];
-	      extn.startWcs->toWorld(point.args.xDevice, point.args.yDevice,
-				     point.args.xExposure, point.args.yExposure);
-	      
-	      Assert(extn.map);
-	      point.map = extn.map;
-	     
-	      point.args.color = 0.;
-	      // Initialize magOut using the map
-	      point.magOut = point.map->forward(point.magIn, point.args);
-	      refs.push_back(point);
-
-	      // Need a second reference point with different color:
-	      // to force any color dependence to agree.
-	      point.args.color = 1.;
-	      point.magOut = point.map->forward(point.magIn, point.args);
-	      refs.push_back(point);
-
-	      // We've found a relevant catalog.
-	      foundOne = true;
-	    } // End section for relevant extension found.
-
-	  } // End loop over exposures matching this input line
-
-	} // End loop over all extensions
-
-	// Warn if exposure not found
-	if (!foundOne) {
-	  cerr << "WARNING: No fitted catalogs found for prior requesting exposure/device "
-	       << exposureRegex << " / " << deviceName
-	       << endl;
+	  matchingXPix.push_back( xpixel<0. ? center.x : xpixel);
+	  matchingYPix.push_back( ypixel<0. ? center.y : ypixel);
 	}
-      } // End processing of an input line specifying an exposure in the prior
+      } // end of loop finding matching exposures
+	    
+      // Troll extensions looking for exposure + device match
+      for (int iExtn = 0; iExtn < extensions.size(); iExtn++) {
+	if (!extensions[iExtn]) continue;
+	Photo::Extension& extn = *extensions[iExtn];
+	bool extnMatch = false;
+	for (int iMatch = 0; !extnMatch && iMatch < matchingExposures.size(); iMatch++) {
+	  if (extn.exposure==matchingExposures[iMatch]
+	      && extn.device==matchingDevices[iMatch]) {
+	    extnMatch = true; // Stop looping - a given extension will match only once
+	    PhotoPriorReferencePoint point;
+	    point.exposureName = exposures[matchingExposures[iMatch]]->name;
+	    point.deviceName = deviceName;
+	    point.magIn = instrumentalMag;
+	    // Check for airmass if needed
+	    point.airmass = extn.airmass;
+	    if ( (airmassIsFree || airmassCoeff!=0.) && point.airmass < 1.) {
+	      cerr << "Prior " << priorName 
+		   << " uses airmass but airmass<1 at exposure " 
+		   << point.exposureName
+		   << " device " << point.deviceName
+		   << endl;
+	      exit(1);
+	    }
+	    point.apcorr = extn.apcorr;
 
-    } // Done reading this prior.
-    out.push_back(new PhotoPrior(refs,
-				 sigma, name,
-				 zeropoint, airmassCoeff, colorCoeff, apcorrCoeff,
-				 zpIsFree, airmassIsFree, colorIsFree, apcorrIsFree));
-  } // No more input from the file.
+	    // Map the coordinates to get [xy]Exposure
+	    Assert(extn.startWcs);
+	    point.args.xDevice = matchingXPix[iMatch];
+	    point.args.yDevice = matchingYPix[iMatch];
+	    extn.startWcs->toWorld(point.args.xDevice, point.args.yDevice,
+				   point.args.xExposure, point.args.yExposure);
+	      
+	    Assert(extn.map);
+	    point.map = extn.map;
+	     
+	    point.args.color = 0.;
+	    // Initialize magOut using the map
+	    point.magOut = point.map->forward(point.magIn, point.args);
+	    refs.push_back(point);
 
+	    // Need a second reference point with different color:
+	    // to force any color dependence to agree.
+	    point.args.color = 1.;
+	    point.magOut = point.map->forward(point.magIn, point.args);
+	    refs.push_back(point);
+
+	  } // End section for relevant extension found.
+
+	} // End loop through exposure/device pairs
+      } // End loop over extensions
+      
+      // Warn if no exposures found
+      if (refs.empty()) {
+	cerr << "WARNING: No fitted catalogs found for prior "
+	     << priorName
+	     << endl;
+      } else {
+	out.push_back(new PhotoPrior(refs,
+				     sigma, priorName,
+				     zeropoint, airmassCoeff, colorCoeff, apcorrCoeff,
+				     zpIsFree, airmassIsFree, colorIsFree, apcorrIsFree));
+      }
+    } // End loop of YAML prior nodes
+
+  } catch (YAML::Exception& e) {
+    cerr << "YAML error during ReadPhotoPrior" << endl;
+    throw;
+  }
   return out;
 }
 
